30f68ca90eb33d4a3823f7be4c28ad23443cf348
Waanz <belanger.jphilippe@gmail.com>
2024-03-15 17:01:40 -0400

    ajout hostname

--- qubic.sh
+++ qubic.sh
@@ -3,3 +3,7 @@
 wget https://dl.qubic.li/downloads/qli-Client-1.8.8-Linux-x64.tar.gz
 gzip -d qli-Client-1.8.8-Linux-x64.tar.gz
 tar qli-Client-1.8.8-Linux-x64.tar qli-Client
+
+h=`hostname`
+
+echo $h
