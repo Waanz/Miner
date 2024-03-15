cc16c9b80215c8b2129c7fbb477817412f48caa2
Waanz <belanger.jphilippe@gmail.com>
2024-03-15 17:18:12 -0400

    Ajout sed hostname et amoutofthreads

--- qubic.sh
+++ qubic.sh
@@ -5,5 +5,12 @@ gzip -d qli-Client-1.8.8-Linux-x64.tar.gz
 tar xf qli-Client-1.8.8-Linux-x64.tar qli-Client
 
 h=`hostname`
+nbr_cpu=$(( `grep -c "^processor"  /proc/cpuinfo` - 1))
 
-echo $h
+
+echo hostname : $h
+echo nbr_cpu : $nbr_cpu
+
+
+sed -i "s/\"amountOfThreads\": 1/\"amountOfThreads\": $nbr_cpu/"t appsettings.json
+sed -i "s/\"alias\": \"qubic.li Client\"/\"alias\": \"$h\"/" appsettings.json
