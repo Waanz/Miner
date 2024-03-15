aacde5f930b8be6ab8462060c50b946049857e17
Waanz <belanger.jphilippe@gmail.com>
2024-03-15 17:03:20 -0400

    maj

--- qubic.sh
+++ qubic.sh
@@ -2,7 +2,7 @@
 
 wget https://dl.qubic.li/downloads/qli-Client-1.8.8-Linux-x64.tar.gz
 gzip -d qli-Client-1.8.8-Linux-x64.tar.gz
-tar qli-Client-1.8.8-Linux-x64.tar qli-Client
+tar xf qli-Client-1.8.8-Linux-x64.tar qli-Client
 
 h=`hostname`
 
