500ae9f9561007c4c0f5298cf40be8ad525c9d95
Waanz <belanger.jphilippe@gmail.com>
2024-03-15 20:37:59 -0400

    ajout du crontab

--- qubic.sh
+++ qubic.sh
@@ -1,21 +1,36 @@
 #
 
 h=`hostname`
-echo "hostname : $h"
 
 wget --no-clobber https://raw.githubusercontent.com/Waanz/Miner/main/host.cfg -O host.cfg
 
 nbr_cpu=$(grep ^$h\; host.cfg  | awk -F\; '{print $2}')
 token=$(grep ^$h\; host.cfg  | awk -F\; '{print $3}' )
 
-echo "nbr_cpu : $nbr_cpu"
-echo "token :$token"
 
 wget https://dl.qubic.li/downloads/qli-Client-1.8.8-Linux-x64.tar.gz
 gzip -f -d qli-Client-1.8.8-Linux-x64.tar.gz
 tar xf qli-Client-1.8.8-Linux-x64.tar qli-Client appsettings.json
 
+echo "Voici la config"
+echo "hostname : $h"
+echo "nbr_cpu : $nbr_cpu"
+echo "token :$token"
+echo 
 
+echo "Ajout au fichier de config"
 sed -i "s/\"accessToken\":.*/\"accessToken\": \"$token\",/" appsettings.json
 sed -i "s/\"amountOfThreads\": 1/\"amountOfThreads\": $nbr_cpu/" appsettings.json
 sed -i "s/\"alias\": \"qubic.li Client\"/\"alias\": \"$h\"/" appsettings.json
+
+echo "Ajout au crontab"
+echo "@reboot cd /home/user ; /usr/bin/screen -dmS qubic sudo /home/user/qli-Client" | crontab
+
+echo Voici le crontab
+crontab -l
+
+cd /home/user
+/usr/bin/screen -dmS qubic sudo /home/user/qli-Client
+
+echo C'est parti
+screen -ls
