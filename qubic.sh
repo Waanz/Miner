1e78b16bc0b9c4ce1683c593792afc03a013653d
Waanz <belanger.jphilippe@gmail.com>
2024-03-15 22:58:23 -0400

    ajout commentaire

--- qubic.sh
+++ qubic.sh
@@ -12,19 +12,20 @@ sudo pkill -e qli-Client
 echo "Kill les screen"
 pkill -e screen
 
+echo "Effacement des fichiers tmp"
 rm -f qli-runner qli-runner.lock
 
-
+#Download Client
 echo "Download le client"
 wget -q https://dl.qubic.li/downloads/qli-Client-1.8.8-Linux-x64.tar.gz
 gzip -f -d qli-Client-1.8.8-Linux-x64.tar.gz
 tar xf qli-Client-1.8.8-Linux-x64.tar qli-Client appsettings.json
 
-
-h=`hostname`
-
+#fichier de config
+echo "Download fichier de config"
 wget -q https://raw.githubusercontent.com/Waanz/Miner/main/hosts.cfg
 
+h=`hostname`
 cpu_true=$(grep ^$h\; hosts.cfg  | awk -F\; '{print $2}')
 nbr_cpu=$(grep ^$h\; hosts.cfg  | awk -F\; '{print $3}')
 gpu_true=$(grep ^$h\; hosts.cfg  | awk -F\; '{print $4}')
@@ -37,7 +38,6 @@ echo "cpu actif: $cpu_true"
 echo "nbr_cpu : $nbr_cpu"
 echo "gpu actif: $gpu_true"
 echo "token :$token"
-echo 
 
 
 if [ $cpu_true == "y" ] ; then 
@@ -49,11 +49,9 @@ if [ $cpu_true == "y" ] ; then
   sed -i "s/\"amountOfThreads\": 1/\"amountOfThreads\": $nbr_cpu/" cpu/appsettings.json
   sed -i "s/\"alias\": \"qubic.li Client\"/\"alias\": \"$h.cpu\"/" cpu/appsettings.json
   cd /home/user/cpu
+  echo "Départ du miner CPU"
   /usr/bin/screen -dmS qubic.cpu sudo ./qli-Client
 
-echo "C'est parti ( screen -ls )"
-
-screen -ls
 fi
 
 if [ $gpu_true_true == "y" ] ; then 
@@ -65,6 +63,7 @@ if [ $gpu_true_true == "y" ] ; then
   sed -i "s/\"amountOfThreads\": 1/\"allowHwInfoCollect\": true/" gpu/appsettings.json
   sed -i "s/\"alias\": \"qubic.li Client\"/\"alias\": \"$h.gpu\"/" gpu/appsettings.json
   cd /home/user/gpu
+  echo "Départ du miner GPU"
   /usr/bin/screen -dmS qubic.gpu sudo ./qli-Client
 fi
 
@@ -75,7 +74,7 @@ echo "@reboot curl -s https://raw.githubusercontent.com/Waanz/Miner/main/qubic.s
 echo Voici le crontab
 crontab -l
 
-pkill screen
+echo "C'est parti ( screen -ls )"
+screen -ls
 
-echo "Départ du miner"
-
+#FIN
