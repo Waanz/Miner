51561044baf61bdffe0788155aa5eb8e5ac9343f
Waanz <belanger.jphilippe@gmail.com>
2024-03-15 20:30:44 -0400

    ajout feature pour wget no redownload

--- qubic.sh
+++ qubic.sh
@@ -3,7 +3,7 @@
 h=`hostname`
 echo "hostname : $h"
 
-wget https://raw.githubusercontent.com/Waanz/Miner/main/host.cfg -O host.cfg
+wget --no-clobber https://raw.githubusercontent.com/Waanz/Miner/main/host.cfg -O host.cfg
 
 nbr_cpu=$(grep ^$h\; host.cfg  | awk -F\; '{print $2}')
 token=$(grep ^$h\; host.cfg  | awk -F\; '{print $3}' )
@@ -13,9 +13,9 @@ echo "token :$token"
 
 wget https://dl.qubic.li/downloads/qli-Client-1.8.8-Linux-x64.tar.gz
 gzip -f -d qli-Client-1.8.8-Linux-x64.tar.gz
-tar xf qli-Client-1.8.8-Linux-x64.tar qli-Client
+tar xf qli-Client-1.8.8-Linux-x64.tar qli-Client appsettings.json
 
 
 sed -i "s/\"accessToken\":.*/\"accessToken\": \"$token\",/" appsettings.json
-sed -i "s/\"amountOfThreads\": 1/\"amountOfThreads\": $nbr_cpu/"t appsettings.json
+sed -i "s/\"amountOfThreads\": 1/\"amountOfThreads\": $nbr_cpu/" appsettings.json
 sed -i "s/\"alias\": \"qubic.li Client\"/\"alias\": \"$h\"/" appsettings.json
