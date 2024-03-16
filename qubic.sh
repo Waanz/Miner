7e3b099af2eeb27e3e6cacae2f46fbfb94b708f2
Waanz <belanger.jphilippe@gmail.com>
2024-03-16 17:35:50 -0400

    ajout sleep, reboot trop tot et wget casse

--- qubic.sh
+++ qubic.sh
@@ -73,7 +73,7 @@ fi
 
 
 echo "Ajout au crontab"
-echo "@reboot cd /home/user ; /usr/bin/wget -q https://raw.githubusercontent.com/Waanz/Miner/main/qubic.sh -O qubic.sh ; /usr/bin/sh /home/user/qubic.sh" | crontab
+echo "@reboot sleep 120 ; cd /home/user ; /usr/bin/wget -q https://raw.githubusercontent.com/Waanz/Miner/main/qubic.sh -O qubic.sh ; /usr/bin/sh /home/user/qubic.sh" | crontab
 
 echo Voici le crontab
 crontab -l
